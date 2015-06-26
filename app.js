dd448603dd0146be89b94092cd860be7b4e17f9b
labuk <labuk.web@gmail.com>
2015-06-26 13:06:10 +0200

    Marco de la aplicación
    Proyecto Quiz del Curso NodeJS - Parte 4

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials = require('express-partials');
 
 // Importar enrutadores 
 var routes = require('./routes/index');
@@ -24,6 +25,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(partials());
 
 // Instalar enrutadores
 app.use('/', routes);
