b27006689139a180d33e02b40215441c8eec142a
labuk <labuk.web@gmail.com>
2015-07-08 18:28:07 +0200

    Validación de entradas
    Proyecto Quiz del Curso NodeJs - Parte 11

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var quizController = require('../controllers/quiz_controller');
 
 // GET home page
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz' , errors: []});
 });
 
 // Autoload de comandos con :quizId
@@ -15,15 +15,15 @@ router.param('quizId', quizController.load); //Si existe parametro quizId hace e
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new', quizController.new); //new quiz
+router.post('/quizes/create', quizController.create); //new quiz
 
 // GET author page 
 router.get('/author', function(req, res){
-  res.render('author');
+  res.render('author', {errors: []});
 });
 
-// GET search quiz 
-router.get('/quizes:', function(req, res){
-  res.render('author');
-});
+
+
 
 module.exports = router;
