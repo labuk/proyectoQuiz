00aea76888363a7a198f07932d7c43686bfaeb6a
labuk <labuk.web@gmail.com>
2015-07-28 09:50:26 +0200

    Autenticación y sesión
    Proyecto Quiz del Curso NodeJs - Parte 15

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 // GET home page
 router.get('/', function(req, res) {
@@ -12,7 +13,7 @@ router.get('/', function(req, res) {
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); //Si existe parametro quizId hace el autoload 
 
-// GET quizes pages
+// Definicion de rutas /quizes 
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
@@ -22,9 +23,14 @@ router.get('/quizes/:quizId(\\d+)/edit', quizController.edit); //edit quiz
 router.put('/quizes/:quizId(\\d+)', quizController.update); //put quiz
 router.delete('/quizes/:quizId(\\d+)', quizController.destroy); //delete quiz
 
-// GET comment pages
+// Definicion de rutas /comment
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+
+// Definicion de rutas de session
+router.get('/login', sessionController.new); // formulario login
+router.post('/login', sessionController.create); // crear session
+router.get('/logout', sessionController.destroy); // destruir session
 
 // GET author page 
 router.get('/author', function(req, res){
